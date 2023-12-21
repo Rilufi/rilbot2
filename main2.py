60ef97763e218b3ca1b9a8157edc477c99aa179e
Yuri <xamexavu@hotmail.com>
2023-12-20 21:17:26 -0300

    Create main2.py

---
+++ main2.py
@@ -0,0 +1,21 @@
+from twiiiiter import *
+import traceback
+import os
+
+if __name__ == "__main__":
+    try:
+        username = os.environ["USER2"]
+        password = os.environ["PASS2"]
+        
+        print("Hello world")
+        if forever_loop == True:
+            forever_loop(username, password)
+        else:    
+            main_one(username, password)
+    except Exception as e:
+        print("Bip Bip Elon Musk")
+        if "Message: unknown error: net::ERR_INTERNET_DISCONNECTED" in str(e):
+            print("No connection")
+        else:
+            print("Another type of error")
+            print(traceback.format_exc())
